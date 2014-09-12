70b2b95665366c8194d07aeb158542a9318b2e19
vferries <vincent.ferries@gmail.com>
2014-09-13 01:31:17 +0200

    Some more examples.

--- src/main/java/demo/jdk8/BaseLambdas.java
+++ src/main/java/demo/jdk8/BaseLambdas.java
@@ -1,7 +1,16 @@
 package demo.jdk8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 
 public class BaseLambdas {
@@ -23,4 +32,30 @@ public class BaseLambdas {
 		Objects.requireNonNull(comparator);
 	}
 	
+	
+	public void limitations() {
+		//No variable modification
+		int count = 0;
+		List<String> list = Arrays.asList("a", "b", "c");
+		//list.forEach(s -> count++);
+		
+		list.forEach(s -> {
+			//throw new Exception("exception");
+		});
+
+		list.forEach(s -> {
+			if ("b".equals(s)) {
+				//break;	
+			}
+		});
+		
+		// Impossible to instanciate an abstract as a lambda expression, only interfaces !
+		
+		//java.util.function
+		Function<Integer, String> function = i -> "The number to guess is " + i;
+		Predicate<String> predicate = s -> true;
+		Consumer<Integer> consumer = System.out::println;
+		Supplier<List> supplier = ArrayList::new;
+		BinaryOperator<Integer> add = (i1, i2) -> i1 + i2;
+	}
 }
