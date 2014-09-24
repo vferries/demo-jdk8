25eca16e5f54be8f82a38c37731a74a4ccf4173d
vferries <vincent.ferries@gmail.com>
2014-09-24 21:55:07 +0200

    Import inutile.

--- src/main/java/demo/jdk8/BaseLambdas.java
+++ src/main/java/demo/jdk8/BaseLambdas.java
@@ -2,7 +2,6 @@ package demo.jdk8;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
